5c6111b0d593321253014ec6d9767cdd97485fee
vivianaranha <vivianaranha@gmail.com>
2016-07-27 17:16:00 -0400

    Task 2 Linking Results Activity

--- WTF-Where'sTheFood/app/src/main/java/com/vivianaranha/wtf_wheresthefood/SearchActivity.java
+++ WTF-Where'sTheFood/app/src/main/java/com/vivianaranha/wtf_wheresthefood/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.vivianaranha.wtf_wheresthefood;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    public void search(View view) {
+        Intent resultsIntents = new Intent(this, ResultsActivity.class);
+        
+        startActivity(resultsIntents);
     }
 }
